8fe6619634c9fc3dcea2cadf0040fbb392c69191
CookieCrackerekt <afiflsils@gmail.com>
2025-04-15 20:59:09 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LatihanController;
+use App\Http\Controllers\AnggotaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('anggota', AnggotaController::class);
 Route::get('gettable', [LatihanController::class, 'getTable']);
 Route::get('getform', [LatihanController::class, 'getForm']);
